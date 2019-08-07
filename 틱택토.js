017ec82607a369ed13533ad03280946bcf07d49d
wrkbr <zcarc1@gmail.com>
2019-08-08 02:45:16 +0900

    5-2 이차원 배열

--- 틱택토.js
+++ 틱택토.js
@@ -4,14 +4,27 @@ console.log('틱택토.js...');
 
 var table = document.createElement('table');
 
+var lines = [];
+var blanks = [];
+
+
+var aync_callback = function(event){
+    console.log('event.target: ', event.target);
+};
+
 
 for(var i = 1; i <= 3; i+= 1) {
 
     var line = document.createElement('tr');
 
+    lines.push(line);
+    blanks.push([]);
+
     for(var j = 1; j <= 3; j += 1) {
 
         var blank = document.createElement('td');
+        blank.addEventListener('click', aync_callback);
+        blanks[i - 1].push(blank);
         line.appendChild(blank);
     }
 
@@ -20,3 +33,6 @@ for(var i = 1; i <= 3; i+= 1) {
 }
 
 document.body.appendChild(table);
+
+console.log(blanks);
+console.log(lines);
