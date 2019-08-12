9ded4eaf623d0c093b7f500afa1c863e3e5b4125
wrkbr <zcarc1@gmail.com>
2019-08-13 05:27:27 +0900

    6-5. JS로 CSS조작하기

--- 로또.js
+++ 로또.js
@@ -3,14 +3,14 @@
     6-1. 로또추첨기 Array & fill
 */
 
-// var 후보군 = Array(45); // [] 배열 45개
-// console.log(후보군);
+// var CandidateArray = Array(45); // [] 배열 45개
+// console.log(CandidateArray);
 
 // Array로 통한 배열 생성 시 empty가 들어있는데
 
 // 첫번째 forEach
-// 후보군.forEach(function (element){
-//     console.log('후보군 forEach');
+// CandidateArray.forEach(function (element){
+//     console.log('CandidateArray forEach');
 //     console.log(element);
 // });
 
@@ -27,14 +27,14 @@
 // undefined의 값들이 각각 들어가도록 설정하려면 fill() 함수를 사용해줘야한다.
 // *IE 브라우저에서는 fill() 함수를 사용하지 못한다...
 
-// var 필 = 후보군.fill();
+// var 필 = CandidateArray.fill();
 // console.log('필: ', 필);
 
-// 필.forEach(function(요소, 인덱스){
-//     console.log(요소, 인덱스 + 1);
+// 필.forEach(function(element, index){
+//     console.log(element, index + 1);
 
 
-//     필[인덱스] = 인덱스 + 1;
+//     필[index] = index + 1;
 
     
 // });
@@ -47,18 +47,16 @@
 */
 
 // Array(45): 45개의 empty가 있는 배열이 반환된다.
-//Array.fill(): undefined로 값이 모두 대입된 상태인 배열이 반한된다.
+// Array.fill(): undefined로 값이 모두 대입된 상태인 배열이 반환된다.
 
 // forEach를 쓰는것보다 map() 함수를 쓰는 것이 더 좋다.
-var 후보군 = Array(45)
-        .fill()
-        .map( function(요소, 인덱스 ) {
+var CandidateArray = Array(45).fill().map( function(elements, index ) {
 
-    return 인덱스 + 1;
+    return index + 1;
 
 });
 
-console.log('후보군: ', 후보군);
+console.log('CandidateArray: ', CandidateArray);
 
 
 
@@ -76,40 +74,40 @@ console.log('후보군: ', 후보군);
 // 여기서 기준값이 감소하는 이유는 
 // Array의 배열에서 값을 하나씩 빼면 그때마다 length가 감소되기 때문이다.
 
-var 셔플 = [];
-while(후보군.length > 0) {
+var shuffle = [];
+while(CandidateArray.length > 0) {
 
-    //console.log('후보군.length: ', 후보군.length);
+    //console.log('CandidateArray.length: ', CandidateArray.length);
 
     // Array.splice() 함수는 배열로 반환된다.
-    var 이동값 = 후보군.splice(Math.random() * 후보군.length, 1)[0];
-    셔플.push(이동값);
+    var movedValue = CandidateArray.splice(Math.random() * CandidateArray.length, 1)[0];
+    shuffle.push(movedValue);
 }
 
-// 위의 코드는 셔플이라는 배열에 우리가 만든 후보군의 값들을 랜덤하기 위해서 만든것이고
-// 후보군에서 랜덤하게 값을 가져올 때 마다 셔플에 그 값을 바로 넣음으로써
-// 셔플에서는 45개의 랜덤한 값이 들어갈 수 있다.
+// 위의 코드는 shuffle이라는 배열에 우리가 만든 CandidateArray의 값들을 랜덤하기 위해서 만든것이고
+// CandidateArray에서 랜덤하게 값을 가져올 때 마다 shuffle에 그 값을 바로 넣음으로써
+// shuffle에서는 45개의 랜덤한 값이 들어갈 수 있다.
 // 추가적으로 Math.random() 함수는 우리가 연습할 때는 사용할 수 있지만
 // 엄밀하게 정확한 랜덤한 값은 아니기 때문에 실전에서는 정확한 알고리즘으로 만들어야한다.
-console.log('셔플: ', 셔플);
+console.log('shuffle: ', shuffle);
 
 
 /*
     6-3. 배열 slice & sort
  */
 
-var 보너스 = 셔플[셔플.length -1];
-var 당첨숫자들 = 셔플.slice(0, 6);
+var bonusNumber = shuffle[shuffle.length -1];
+var lotteryNumbers = shuffle.slice(0, 6);
 
 // sort() 함수는 정렬을 해주기는 하는데 제일 앞에 있는 숫자 기준으로 오름차순 정렬을 해준다.
-console.log('당첨숫자들: ', 당첨숫자들.sort(), ' 보너스: ', 보너스);
+console.log('lotteryNumbers: ', lotteryNumbers.sort(), ' bonusNumber: ', bonusNumber);
 
 // 정확하게 오름차순을 하고 싶다면 아래와 같이 하면 된다.
 console.log('정확한 오름차순');
-console.log('당첨숫자들: ', 
-    당첨숫자들.sort(function(p, c){
+console.log('lotteryNumbers: ', 
+    lotteryNumbers.sort(function(p, c){
         return p - c}), 
-    ' 보너스: ', 보너스);
+    ' bonusNumber: ', bonusNumber);
 
  
 // p: 이전 숫자
@@ -131,7 +129,7 @@ console.log('당첨숫자들: ',
 
 // 정확하게 내림차순을 하고 싶다면 아래와 같이 한다.
 console.log('정확한 내림차순');
-console.log('당첨숫자들: ', 당첨숫자들.sort(function(p, c){return p - c}), ' 보너스: ', 보너스);
+console.log('lotteryNumbers: ', lotteryNumbers.sort(function(p, c){return p - c}), ' bonusNumber: ', bonusNumber);
 
 
 // <내림차순 정렬>
@@ -159,38 +157,67 @@ console.log('당첨숫자들: ', 당첨숫자들.sort(function(p, c){return p - 
         6-4. JS로 HTML 태그 선택하기
  */
 
- var 결과창 = document.getElementById('결과창');
-
- for(var i = 0; i < 당첨숫자들.length; i+= 1) {
-
-    setTimeout(function 비동기콜백함수() {
-
-        var 공 = document.createElement('div');
-        공.textContent = 당첨숫자들[i];
-        결과창.append(공);
-    }, 1000);
-
-    
- }
-
-// class는 같은 이름을 여러개 사용할 수 있어서 배열로 가져와야한다.
-var 보너스칸 = document.getElementsByClassName('보너스')[0];
-
-var 보너스공 = document.createElement('div');
-보너스공.textContent = 보너스;
-보너스칸.append(보너스공);
 
 
 // 아래 코드는 클로저(closure) 문제라 반복문 안에 비동기를 사용하는 것은 
 // 클로저를 배운 후에 알아본다.
-// for(var i = 0; i < 당첨숫자들.length; i+= 1) {
+// 이러한 상황은 자바스크립트에서 유명하다.
+// for(var i = 0; i < lotteryNumbers.length; i+= 1) {
 
 //     setTimeout(function 비동기콜백함수() {
 
 //         var 공 = document.createElement('div');
-//         공.textContent = 당첨숫자들[i];
+//         공.textContent = lotteryNumbers[i];
 //         결과창.append(공);
 //     }, 1000);
 
     
 //  }
+
+
+
+/*
+        6-5. JS로 CSS 조작하기
+ */
+
+var LotteryNumbersDiv = document.getElementById('LotteryNumbersDiv');
+console.log('LotteryNumbersDiv: ', LotteryNumbersDiv);
+
+ function PaintBalls(textNumber, printDiv) {
+
+    
+    var createdDiv = document.createElement('div');
+    createdDiv.textContent = textNumber;
+    createdDiv.style.display = 'inline-block';
+    createdDiv.style.border = '1px solid black';
+    createdDiv.style.borderRadius = '10px';
+    createdDiv.style.width = '20px';
+    createdDiv.style.height = '20px';
+    createdDiv.style.textAlign = 'center';
+    createdDiv.style.marginRight = '10px';
+    printDiv.append(createdDiv);
+ }
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[0], LotteryNumbersDiv); 
+}, 1000);
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[1], LotteryNumbersDiv);
+}, 2000);
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[2], LotteryNumbersDiv);
+}, 3000);
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[3], LotteryNumbersDiv);
+}, 4000);
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[4], LotteryNumbersDiv);
+}, 5000);
+setTimeout(function aynchronous_Callback() {
+    PaintBalls(lotteryNumbers[5], LotteryNumbersDiv);
+}, 6000); 
+
+setTimeout(function aynchronous_Callback(){
+    // class는 같은 이름을 여러개 사용할 수 있어서 배열로 가져와야한다.
+    var bonusNumberDiv = document.getElementsByClassName('BonusNumberDiv')[0];
+    PaintBalls(bonusNumber, bonusNumberDiv);
+}, 7000);
